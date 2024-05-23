12057f96d8bc1670234fe294de7d50d788f3ad4f
sembur_art <102034554+abhinayafl@users.noreply.github.com>
2024-05-24 03:02:43 +0700

    create api temperature
    Create Controller, Model, Routes untuk POST  dan GET

---
+++ app/Http/Controllers/Api/TemperatureController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Temperature;
+
+class TemperatureController extends Controller
+{
+    public function store(Request $request) // Ini adalah fungsi / method untuk menyimpan data temperatur ke dalam database, penaamn methodnya hanya contoh saja
+    {
+        $request->validate([ // Memberikan validasi pada inputan
+            'value' => 'required|numeric', // Validasi inputan value harus berupa angka
+        ]);
+
+        $temperature = Temperature::create($request->all()); // Menyimpan data temperatur ke dalam database
+
+        return response()->json($temperature, 201);  // Memberikan response berupa data temperatur yang telah disimpan ke dalam database
+    }
+
+     // Metode untuk mengambil semua data temperatur
+     public function index()
+     {
+         $temperatures = Temperature::all(); // Mengambil semua data temperatur dari database
+         return response()->json($temperatures); // Mengembalikan response berupa JSON dari semua data temperatur
+     }
+
+     public function show($id)
+    {
+        $temperature = Temperature::find($id); // Mencari data temperatur berdasarkan ID
+
+        if (!$temperature) { // Jika data temperatur tidak ditemukan
+            return response()->json(['message' => 'Temperature not found'], 404); // Mengembalikan response error 404
+        }
+
+        return response()->json($temperature); // Mengembalikan response berupa JSON dari data temperatur yang ditemukan
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\TemperatureController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,20 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// -----------CONTROLLER SENSOR------------------------
+
+//--------------------------------------------------------------------------------
+
+// Controller dari temperature
+
+// Route GET untuk mengambil semua data temperatur
+Route::post('/temperatures', [TemperatureController::class, 'store']);
+
+// Route GET untuk mengambil semua data temperatur
+Route::get('/temperatures', [TemperatureController::class, 'index']);
+
+// Route GET untuk mengambil data temperatur berdasarkan ID
+Route::get('/temperatures/{id}', [TemperatureController::class, 'show']);
+
+//--------------------------------------------------------------------------------
